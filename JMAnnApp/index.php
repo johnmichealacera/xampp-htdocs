8523b14dc7e96dd5c693a2faaa06e3d581177561
JMAnn432015 <jmannacera@gmail.com>
2020-01-30 07:39:08 +0800

    Initial Version

---
+++ JMAnnApp/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>My First Social Media</title>
+</head>
+<body>
+<div id="couple">
+<img src="images/couple.jpg" alt="couple picture"/>
+</div>
+<nav id="login">
+<ul>
+    <li>Username: <input type="text" name="username"></li>
+    <li>Password: <input type="password" name="password"></li>
+    <li><input type="submit" value="login"></li>
+</ul>
+</nav>
+<div id="signup">
+<form id="create">
+<h1>Create Account</h1>
+Create Username: <input type="text" name="createUser"><br>
+Create Password: <input type="password" name="createPass"><br>
+Verify Password: <input type="password" name="verifyPass"><br>
+<input type="submit" value="Create Account">
+</form>
+</div>
+</body>
+</html>
