5e47a8563b891634318a2853f8a2dc0a94456c7b
JMAnn432015 <jmannacera@gmail.com>
2020-01-30 10:29:54 +0800

    First Phase 1st Half

--- JMAnnApp/index.php
+++ JMAnnApp/index.php
@@ -3,6 +3,7 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <title>My First Social Media</title>
+    <script type="text/javascript" src="function.js"></script>
 </head>
 <body>
 <div id="couple">
@@ -11,18 +12,18 @@
 <nav id="login">
 <form>
 <ul>
-    <li>Username: <input type="text" name="username"></li>
-    <li>Password: <input type="password" name="password"></li>
+    <li><input type="text" name="username" placeholder="Username" required></li>
+    <li><input type="password" name="password" placeholder="Password" required></li>
     <li><input type="submit" value="login" id="btnLogin"></li>
 </ul>
 </form>
 </nav>
 <div id="signup">
-<form id="create">
+<form id="create" name="myForm" onsubmit="return confirmPass()" action="createAccount.php" method="post">
 <h1>Create Account</h1>
-Create Username: <input type="text" name="createUser"><br><br><br>
-Create Password: <input type="password" name="createPass"><br><br><br>
-Verify Password: <input type="password" name="verifyPass"><br><br><br>
+Create Username: <input type="text" name="createUser" required><br><br><br>
+Create Password: <input type="password" name="createPass" required><br><br><br>
+Verify Password: <input type="password" name="verifyPass" required><br><br><br>
 <input type="submit" value="Create Account" id="btnCreate">
 </form>
 </div>
