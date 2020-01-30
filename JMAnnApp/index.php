1f3f038ab19605e1215e835cc0d7a4c383ffed03
JMAnn432015 <jmannacera@gmail.com>
2020-01-30 08:25:23 +0800

    First Phase

--- JMAnnApp/index.php
+++ JMAnnApp/index.php
@@ -9,19 +9,21 @@
 <img src="images/couple.jpg" alt="couple picture"/>
 </div>
 <nav id="login">
+<form>
 <ul>
     <li>Username: <input type="text" name="username"></li>
     <li>Password: <input type="password" name="password"></li>
-    <li><input type="submit" value="login"></li>
+    <li><input type="submit" value="login" id="btnLogin"></li>
 </ul>
+</form>
 </nav>
 <div id="signup">
 <form id="create">
 <h1>Create Account</h1>
-Create Username: <input type="text" name="createUser"><br>
-Create Password: <input type="password" name="createPass"><br>
-Verify Password: <input type="password" name="verifyPass"><br>
-<input type="submit" value="Create Account">
+Create Username: <input type="text" name="createUser"><br><br><br>
+Create Password: <input type="password" name="createPass"><br><br><br>
+Verify Password: <input type="password" name="verifyPass"><br><br><br>
+<input type="submit" value="Create Account" id="btnCreate">
 </form>
 </div>
 </body>
