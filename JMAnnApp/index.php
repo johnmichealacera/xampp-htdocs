3caa3cbf9e69cb43a523fb03083c850c6ba1477e
JMAnn432015 <jmannacera@gmail.com>
2020-01-31 08:06:17 +0800

    Added Iframe

--- JMAnnApp/index.php
+++ JMAnnApp/index.php
@@ -12,7 +12,7 @@
 <nav id="login">
 <form action="login.php" method="post">
 <ul>
-    <li><input type="text" name="username" placeholder="Username" required></li>
+    <li><input type="text" name="username" placeholder="Username" autofocus="autofocus" required></li>
     <li><input type="password" name="pword" placeholder="Password" required></li>
     <li><input type="submit" value="login" id="btnLogin"></li>
 </ul>
@@ -21,9 +21,9 @@
 <div id="signup">
 <form id="create" name="myForm" onsubmit="return confirmPass()" action="createAccount.php" method="post">
 <h1>Create Account</h1>
-Create Username: <input type="text" name="createUser" required><br><br><br>
-Create Password: <input type="password" name="createPass" required><br><br><br>
-Verify Password: <input type="password" name="verifyPass" required><br><br><br>
+<label for="createUser">Create Username: </label><input type="text" name="createUser" required><br><br><br>
+<label for="createPass">Create Password: </label><input type="password" name="createPass" required><br><br><br>
+<label for="verifyPass">Verify Password: <label><input type="password" name="verifyPass" required><br><br><br>
 <input type="submit" value="Create Account" id="btnCreate">
 </form>
 </div>
