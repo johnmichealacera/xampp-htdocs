69efe66e0f276a1086ffc9e2e79f6e0310336f93
JMAnn432015 <jmannacera@gmail.com>
2020-02-01 16:06:17 +0800

    Upload Function

---
+++ JMAnnApp/superDashboard.php
@@ -0,0 +1,9 @@
+<html>
+<body style="background-color:pink;">
+
+<h1 style="text-align:center;font-style: italic;font-family: arial, Times, serif;">This is the super Dashboard</h1><br><br>
+<button onclick="window.location='superProfile.php';">View Users/Add Book Information</button><br><br>
+<button>View Book Information</button>
+
+</body>
+</html>
