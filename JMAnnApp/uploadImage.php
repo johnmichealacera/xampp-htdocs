c74b4e803a081ee8bc373ecdca6e9bebaeb586ba
JMAnn432015 <jmannacera@gmail.com>
2020-02-01 12:20:18 +0800

    Upload Function

---
+++ JMAnnApp/uploadImage.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form>
+<label for="uploadImage">Upload Image</label>
+<input type="file" name="uploadImage"><br><br>
+<input type="submit" value="Upload">
+</form>
+<?php
+
+
+?>
+</body>
+</html>
