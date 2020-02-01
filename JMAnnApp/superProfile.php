6927bebcb5d03f310e880c25c6bf7d03a0b00701
JMAnn432015 <jmannacera@gmail.com>
2020-02-01 15:13:17 +0800

    Upload Function

--- JMAnnApp/superProfile.php
+++ JMAnnApp/superProfile.php
@@ -3,10 +3,10 @@
 <link rel="stylesheet" href="superStyle.css">
 <title>Super User Profile</title>
 </head>
-<body>
+<body >
 <div id="superFunction" class="main">
-<h1>Welcome</h1>
-<h2>Please select what do you want to do.</h2>
+<h1 >Welcome</h1>
+<h2 >Please select what do you want to do.</h2>
 <a href="showUsers.php" target="iframeShow">View all users</a><br><br>
 <a href="addAuthor.html" target="iframeShow">Add Author</a><br><br>
 <a href="addBook.php" target="iframeShow">Add Book</a><br>
