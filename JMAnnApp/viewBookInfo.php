5ae26bf92b0bc951f399718dc882126d86d08227
JMAnn432015 <jmannacera@gmail.com>
2020-02-02 07:13:23 +0800

    View Title Function

--- JMAnnApp/viewBookInfo.php
+++ JMAnnApp/viewBookInfo.php
@@ -1,13 +1,17 @@
 <html>
 <body style="background-color:skyblue;">
 <nav style="float:right;">
+<form action="viewFrame.php" method="post">
 <label for="searchBy">Search Book By</label>
 <select name="searchBy" required>
-    <option>Title</option>
-    <option>Author</option>
+    <option value="title">Title</option>
+    <option value="author">Author</option>
 </select>
 <input type="search" name="bookSearch">
 <input type="submit" value="Enter">
+</form>
 </nav>
+<div>
+<iframe src="viewFrame.php" width="100%" height="100%"></iframe>
 </body>
 </html>
---
+++ JMAnnApp/viewFrame.php
@@ -0,0 +1,45 @@
+<html>
+<body style="background-color: burlywood;">
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "socialmedia";
+$selection = $_POST['searchBy'];
+$bookInfo = $_POST['bookSearch'];
+
+$conn = new mysqli($servername, $username, $password, $dbName);
+if($conn->connect_error){
+    die("Connection failed ".$conn->connect_error);
+}
+else{
+    if($selection=="title"){
+    $sql = "select title, author_id, folder, upload_image, publish, plot from books where title = '$bookInfo'";
+    $result = $conn->query($sql);
+        if($result->num_rows>0){
+            ?>
+            <ul>
+            <?php
+            while($row=$result->fetch_assoc()){
+                ?>
+                <img src="http://localhost/C:/xampp/htdocs/JMAnnApp/images/images (8).jpeg" alt="Book Image" width="300" height="300">
+                <li><?php echo $row['title'];?></li>
+                <li><?php echo $row['publish'];?></li>
+                <li><?php echo $row['plot']; ?></li>
+                <li><?php echo $row['folder'].$row['upload_image']; ?></li>
+                <?php
+            }
+            ?>
+            </ul>
+            <?php
+        }
+        else{
+            echo "No books matched. ";
+        }
+    }
+}
+
+$conn->close();
+?>
+</body>
+<html>
