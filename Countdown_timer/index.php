3d447646fa56548ee4cf3b01dcce6b0117acb12f
JMAnn432015 <jmannacera@gmail.com>
2020-02-19 11:03:58 +0800

    withEventTimer

---
+++ Countdown_timer/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap-4.4.1-dist\css\bootstrap.min.css">
+    <title>Event Countdown</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row pt-3">
+            <div class="col">
+            <h1 class="display-1 text-center"> Event Countdown</h1>
+            </div>
+        </div>
+        <div class="row pt-4">
+            <input class="col-6" type="text" placeholder="Event Name">
+            <input class="col-3" type="date">
+            <input class="col-3" type="time">
+        </div>
+        <div class="row pt-4">
+        <input class="col-6" type="submit" value="Create Event">
+        </div>
+    </div>
+</body>
+</html>
